831f32e0e2bb2ca37c72cc8a0b7dd38a15c81bc6
Khadiev Edem <khadiev.edem@gmail.com>
2025-05-09 23:07:48 +0300

    feat: Add modbus to the D1 build

--- Eco.SBv1.D1PM/SourceFiles/EcoPowerManagement1.c
+++ Eco.SBv1.D1PM/SourceFiles/EcoPowerManagement1.c
@@ -24,11 +24,26 @@
 #include "IEcoCommandArguments1.h"
 #include "IdEcoMemoryManager1.h"
 #include "IdEcoFileSystemManagement1.h"
-#include "IdEcoList1.h"
+/* #include "IdEcoList1.h" */
+
+/* ModBus */
+#include "IdEcoModBus1.h"
+#include "IdEcoModBus1SL.h"
+#include "IEcoModBus1SLRTU.h"
+
+/* Log */
+#include "IdEcoLog1.h"
+#include "IEcoLog1ConsoleAffiliate.h"
 
 
 /* Глобальный указатель на системный интерфейс */
 IEcoSystem1* g_pISys = 0;
+
+/* Глобальный указатель на интерфейс работы с логами */
+static IEcoLog1* g_pILog = 0;
+
+enum eEcoLog1Level LogLevel = ECO_LOG_1_LEVEL_DEBUG || ECO_LOG_1_LEVEL_ERROR;
+
 
 /*
  *
@@ -49,14 +64,16 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     IEcoMemoryAllocator1* pIMem = 0;
     IEcoFileManager1* pIFileMgr = 0;
     IEcoFile1* pIFile = 0;
-    IEcoList1* pIList = 0;
-    char_t* pszBuffer = 0;
-    uint32_t iSize = 0;
-    uint32_t iIndex = 0;
+    IEcoLog1Affiliate* pIConsoleAffiliate = 0;
+    /* Указатель на интерфейс работы с ModBus */
+    IEcoModBus1* pIEcoModBus1 = 0;
+    /* Указатели на тестируемые интерфейсы */
+    IEcoModBus1SLRTU*   pIEcoModBus1SLRTU = 0;
+
 
     /* Проверка и создание системного интрефейса */
     if (g_pISys == 0) {
-        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem, (void **)&g_pISys);
+        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem1, (void **)&g_pISys);
         if (result != 0 || g_pISys == 0) {
         /* Освобождение системного интерфейса в случае ошибки */
             goto Release;
@@ -79,22 +96,36 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
         goto Release;
     }
 
-    /* Выделение блока памяти */
-    pszBuffer = (char_t *)pIMem->pVTbl->Alloc(pIMem, 10);
-
-    /* Заполнение блока памяти */
-    pIMem->pVTbl->Fill(pIMem, pszBuffer, 'a', 9);
-
-    /* Освлбождение блока памяти */
-    pIMem->pVTbl->Free(pIMem, pszBuffer);
 
 #ifdef ECO_LIB
-    /* Регистрация статического компонента для работы со списком */
-    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoList1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_53884AFC93C448ECAA929C8D3A562281);
+    /* Регистрация статического компонента для работы с UART */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoUART1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_B40E129B56624BD7B5F8339C025F3EF0);
     if (result != 0 ) {
         /* Освобождение в случае ошибки */
         goto Release;
     }
+
+    /* Регистрация статического компонента для работы с ModBus1 */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoModBus1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_D3D7232DDB6940469D2B535BA10E8757);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+
+    /* Регистрация статического компонента для работы с ModBus1 Serial Line */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoModBus1SL, (IEcoUnknown*)GetIEcoComponentFactoryPtr_DA26D759A46F405F9977CB5AA9153876);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+
+    /* Регистрация статического компонента для работы с Логгером */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoLog1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_97322B6765B74342BBCE38798A0B40B5);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+
 #endif
 
 
@@ -112,24 +143,51 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     pISysInfo->pVTbl->Release(pISysInfo);
     pICmdArg->pVTbl->Release(pICmdArg);
 
-    /* Получение интерфейса для работы со списком */
-    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoList1, 0, &IID_IEcoList1, (void**) &pIList);
-    /* Проверка */
-    if (result != 0 || pIList == 0) {
+    /* Получение интерфейса работы с ModBus */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoModBus1, 0, &IID_IEcoModBus1, (void**) &pIEcoModBus1);
+    if (result != 0 || pIEcoModBus1 == 0) {
+        /* Освобождение интерфейсов в случае ошибки */
+        goto Release;
+    }
+
+    /* Получение интерфейса работы с ModBus.SL */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoModBus1SL, 0, &IID_IEcoModBus1SLRTU, (void**) &pIEcoModBus1SLRTU);
+    if (result != 0 || pIEcoModBus1SLRTU == 0) {
+        /* Освобождение интерфейсов в случае ошибки */
+        goto Release;
+    }
+
+    /* Получение интерфейса логгирования */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoLog1, 0, &IID_IEcoLog1, (void**) &g_pILog);
+    if (result != 0 || g_pILog == 0) {
         /* Освобождение системного интерфейса в случае ошибки */
         goto Release;
     }
+
+    /* Получение компонента вывода в консоль */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoLog1, 0, &IID_IEcoLog1ConsoleAffiliate, (void**)&pIConsoleAffiliate);
+    if (result != 0 || pIConsoleAffiliate == 0) {
+        goto Release;
+    }
+
+    /* Подключение к шине */
+    byte_t devName[] = "/dev/ttyS2";
+    result = pIEcoModBus1SLRTU->pVTbl->ConnectBus(pIEcoModBus1SLRTU, 1, devName, sizeof(devName));
+    if (result != ERR_ECO_SUCCESES) {
+        /* Освобождение интерфейсов в случае ошибки */
+        goto Release;
+    }
+    
+    g_pILog->pVTbl->AddAffiliate(g_pILog, (IEcoLog1Affiliate*)pIConsoleAffiliate);
+    g_pILog->pVTbl->set_LevelMask(g_pILog, LogLevel);
+    g_pILog->pVTbl->DebugFormat(g_pILog, "LogLevel: %d", LogLevel);
+
 
 Release:
 
     /* Освобождение интерфейса управления файлами */
     if (pIFileMgr != 0) {
         pIFileMgr->pVTbl->Release(pIFileMgr);
-    }
-
-    /* Освобождение интерфейса для работы со списком */
-    if (pIList != 0) {
-        pIList->pVTbl->Release(pIList);
     }
 
     /* Освобождение интерфейса работы с памятью */
