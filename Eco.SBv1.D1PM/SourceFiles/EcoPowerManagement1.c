fb41b037b98e661f977f16bd5c4b2093edef9bc3
Khadiev Edem <khadiev.edem@gmail.com>
2025-05-13 22:40:50 +0300

    feat: Update bussiness logic

--- Eco.SBv1.D1PM/SourceFiles/EcoPowerManagement1.c
+++ Eco.SBv1.D1PM/SourceFiles/EcoPowerManagement1.c
@@ -32,8 +32,13 @@
 #include "IEcoModBus1SLRTU.h"
 
 /* Log */
+#include "IdEcoList1.h"
+#include "IdEcoString1.h"
 #include "IdEcoLog1.h"
 #include "IEcoLog1ConsoleAffiliate.h"
+
+/* UART */
+#include "IdEcoUART1.h"
 
 
 /* Глобальный указатель на системный интерфейс */
@@ -42,7 +47,7 @@ IEcoSystem1* g_pISys = 0;
 /* Глобальный указатель на интерфейс работы с логами */
 static IEcoLog1* g_pILog = 0;
 
-enum eEcoLog1Level LogLevel = ECO_LOG_1_LEVEL_DEBUG || ECO_LOG_1_LEVEL_ERROR;
+enum eEcoLog1Level LogLevel = ECO_LOG_1_LEVEL_DEBUG | ECO_LOG_1_LEVEL_ERROR;
 
 
 /*
@@ -73,8 +78,9 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
 
     /* Проверка и создание системного интрефейса */
     if (g_pISys == 0) {
-        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem1, (void **)&g_pISys);
+        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem, (void **)&g_pISys);
         if (result != 0 || g_pISys == 0) {
+	printf("EcoSystem error\n");
         /* Освобождение системного интерфейса в случае ошибки */
             goto Release;
         }
@@ -83,6 +89,7 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     /* Получение интерфейса для работы с интерфейсной шиной */
     result = g_pISys->pVTbl->QueryInterface(g_pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
     if (result != 0 || pIBus == 0) {
+	printf("EcoInterfaceBus error\n");
         /* Освобождение в случае ошибки */
         goto Release;
     }
@@ -92,6 +99,7 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
 
     /* Проверка */
     if (result != 0 && pIMem == 0) {
+	printf("EcoMemoryAlloc error\n");
         /* Освобождение системного интерфейса в случае ошибки */
         goto Release;
     }
@@ -119,6 +127,22 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
         goto Release;
     }
 
+
+    /* Регистрация статического компонента для работы со списком */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoList1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_53884AFC93C448ECAA929C8D3A562281);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+
+
+    /* Регистрация статического компонента для работы со строками */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoString1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_84CC0A7DBABD44EEBE749C9A8312D37E);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+
     /* Регистрация статического компонента для работы с Логгером */
     result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoLog1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_97322B6765B74342BBCE38798A0B40B5);
     if (result != 0 ) {
@@ -127,25 +151,27 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     }
 
 #endif
+    printf("Registration OK\n");
 
+    // result = g_pISys->pVTbl->QueryInterface(g_pISys, &IID_IEcoSystemInformation1, (void **)&pISysInfo);
+    // if (result != 0 || pISysInfo == 0) {
+    //     /* Освобождение в случае ошибки */
+    //     goto Release;
+    // }
 
-    result = g_pISys->pVTbl->QueryInterface(g_pISys, &IID_IEcoSystemInformation1, (void **)&pISysInfo);
-    if (result != 0 || pISysInfo == 0) {
-        /* Освобождение в случае ошибки */
-        goto Release;
-    }
-
-    result = pISysInfo->pVTbl->QueryInterface(pISysInfo, &IID_IEcoCommandArguments1, (void **)&pICmdArg);
-    if (result != 0 || pICmdArg == 0) {
-        /* Освобождение в случае ошибки */
-        goto Release;
-    }
-    pISysInfo->pVTbl->Release(pISysInfo);
-    pICmdArg->pVTbl->Release(pICmdArg);
+    // result = pISysInfo->pVTbl->QueryInterface(pISysInfo, &IID_IEcoCommandArguments1, (void **)&pICmdArg);
+    // if (result != 0 || pICmdArg == 0) {
+    //     /* Освобождение в случае ошибки */
+    //     printf("EcoCMD error\n");
+    //     goto Release;
+    // }
+    // pISysInfo->pVTbl->Release(pISysInfo);
+    // pICmdArg->pVTbl->Release(pICmdArg);
 
     /* Получение интерфейса работы с ModBus */
     result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoModBus1, 0, &IID_IEcoModBus1, (void**) &pIEcoModBus1);
     if (result != 0 || pIEcoModBus1 == 0) {
+	printf("EcoModBus error\n");
         /* Освобождение интерфейсов в случае ошибки */
         goto Release;
     }
@@ -153,6 +179,7 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     /* Получение интерфейса работы с ModBus.SL */
     result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoModBus1SL, 0, &IID_IEcoModBus1SLRTU, (void**) &pIEcoModBus1SLRTU);
     if (result != 0 || pIEcoModBus1SLRTU == 0) {
+	printf("EcoModBus RTU error\n");
         /* Освобождение интерфейсов в случае ошибки */
         goto Release;
     }
@@ -160,6 +187,7 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     /* Получение интерфейса логгирования */
     result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoLog1, 0, &IID_IEcoLog1, (void**) &g_pILog);
     if (result != 0 || g_pILog == 0) {
+	printf("EcoLog error\n");
         /* Освобождение системного интерфейса в случае ошибки */
         goto Release;
     }
@@ -167,13 +195,15 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     /* Получение компонента вывода в консоль */
     result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoLog1, 0, &IID_IEcoLog1ConsoleAffiliate, (void**)&pIConsoleAffiliate);
     if (result != 0 || pIConsoleAffiliate == 0) {
+	printf("EcoLog affiliate error\n");
         goto Release;
     }
 
-    /* Подключение к шине */
+    /* Подключение к UART2 */
     byte_t devName[] = "/dev/ttyS2";
     result = pIEcoModBus1SLRTU->pVTbl->ConnectBus(pIEcoModBus1SLRTU, 1, devName, sizeof(devName));
     if (result != ERR_ECO_SUCCESES) {
+	printf("Device not connected\n");
         /* Освобождение интерфейсов в случае ошибки */
         goto Release;
     }
@@ -181,6 +211,49 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     g_pILog->pVTbl->AddAffiliate(g_pILog, (IEcoLog1Affiliate*)pIConsoleAffiliate);
     g_pILog->pVTbl->set_LevelMask(g_pILog, LogLevel);
     g_pILog->pVTbl->DebugFormat(g_pILog, "LogLevel: %d", LogLevel);
+
+    uint8_t functionCode = ECO_MB_FC_WRITE_FILE_RECORD;
+    uint16_t subCode = 0;
+
+    uint32_t dataLength;
+    byte_t* recvData = 0;
+
+    while (1) {
+    	g_pILog->pVTbl->DebugFormat(g_pILog, "Receiving...");
+        result = pIEcoModBus1SLRTU->pVTbl->RecvMessage(pIEcoModBus1SLRTU, &functionCode, &subCode, &recvData, &dataLength);
+        // if (result != ERR_ECO_SUCCESES) {
+        //     g_pILog->pVTbl->ErrorFormat(g_pILog, "%d Error while receiving", __LINE__);
+        // }
+	if (dataLength)
+	{
+		printf("datalength: %d\n ---", dataLength);
+		for (int i = 0; i < dataLength; i++)
+		{
+			printf("%c", (char)recvData[i]);
+		}
+		printf("\n");
+	}
+        
+        switch (functionCode) {
+        case ECO_MB_FC_WRITE_FILE_RECORD:
+            functionCode = ECO_MB_FC_WRITE_FILE_RECORD;
+            subCode = 0;
+
+            g_pILog->pVTbl->DebugFormat(g_pILog, "[LINE: %d] ----- HALT received -----", __LINE__);
+            //byte_t sendData[] = " HALT";
+            //uint32_t dataLength = sizeof(sendData);
+            //sendData[0] = (uint8_t)(dataLength - 1);
+            //result = pIEcoModBus1SLRTU->pVTbl->EmitMessage(pIEcoModBus1SLRTU, functionCode, subCode, sendData, dataLength);
+            //if (result != ERR_ECO_SUCCESES) {
+            //    g_pILog->pVTbl->ErrorFormat(g_pILog, "%d Error while emmiting", __LINE__);
+            //}
+            break;
+        default:
+            break;
+        }
+
+        free(recvData);
+    }
 
 
 Release:
